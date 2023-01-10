c9a600aa82038e76d353271d4e91f6154f0f9434
Chit Ye Aung <chityeaung.tu@gmail.com>
2023-01-10 19:49:56 +0630

    ADD: analysis options

--- lib/app_initializer.dart
+++ lib/app_initializer.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/adapters.dart';
 
 Future<void> initializeApp() async {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
   );
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
 }
